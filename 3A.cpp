e5f8e6178132ce5be0a8c085e908fe815c33e782
farisimbaa <146722699+farisimbaa@users.noreply.github.com>
2026-09-14 10:50:50 +0800

    Update 3A.cpp

--- 3A.cpp
+++ 3A.cpp
@@ -5,12 +5,37 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
  
-    string ans;
     string start, end;
     cin >> start >> end;
-    
-    
- 
-    cout << ans << endl;
+
+    int dx = abs(start[0] - end[0]);
+    int dy = abs(start[1] - end[1]);
+
+    cout << max(dx,dy) << endl;
+
+    string horizontal;
+    if (start[0] < end [0]) horizontal = "R";
+    else if (start[0] > end[0]) horizontal = "L";
+
+    string vertical;
+    if (start[1] < end[1]) vertical = "U";
+    else if (start[1] > end[1]) vertical = "D";
+
+    while (dx > 0 && dy > 0) {
+        cout << horizontal + vertical << endl;
+        dx--;
+        dy--;
+    }
+
+    while (dx > 0) {
+        cout << horizontal << endl;
+        dx--;
+    }
+
+    while (dy > 0) {
+        cout << vertical << endl;
+        dy--;
+    }
+
     return 0;
 }
