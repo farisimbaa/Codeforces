81e4d05d41df1014d4f6aae00625919282ed6ccb
farisimbaa <146722699+farisimbaa@users.noreply.github.com>
2026-09-14 12:04:14 +0800

    Create 1C.cpp

---
+++ 1C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Point {
+    double x, y;
+};
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Point p1, p2, p3;
+    cin >> p1.x >> p1.y;
+    cin >> p2.x >> p2.y;
+    cin >> p3.x >> p3.y;
+}
