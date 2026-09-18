2d33382d5507ee46db99f55d2098667cb0c571dc
farisimbaa <146722699+farisimbaa@users.noreply.github.com>
2026-09-18 09:08:18 +0800

    solved 281A

---
+++ 281A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string input; cin >> input;
+
+    input[0] = toupper(input[0]);
+    cout << input << endl;
+    return 0;
+}
